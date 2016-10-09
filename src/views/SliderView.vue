3e9cc9257dd791ec2d16d386acc6b7344d94feb0
John Leider <john.j.leider@gmail.com>
2016-10-08 20:26:42 -0400

    updated slider view

--- src/views/SliderView.vue
+++ src/views/SliderView.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
   doc-view(v-bind:doc="doc")
     div#slider
-      v-slider(v-model="current")
+      v-slider
         v-slider-item(
           v-for="(item, index) in items"
           v-bind:src="item.src"
@@ -12,85 +12,12 @@
   div#slider {
     padding: 0 2rem
   }
-  
-  .wrapper
-    overflow: hidden
-    margin: 0 2rem
-    
-  .carousel
-    margin: 0
-    padding: 0
-    display: flex
-    position: relative
-    left: -33.33%
-    transform: translateX(33.33%)
-      
-    &--is-reversing
-      transform: translateX(-33.33%)
-    
-    &--is-set
-      transform: none
-      transition: transform 0.5s cubic-bezier(0.23, 1, 0.32, 1)
-    
-    &__item
-      display: flex
-      align-items: center
-      justify-content: center
-      background: rgba(grey, .4)
-      flex: 1 0 33.33%
-      height: 200px
-      order: 2
-      
-      &--is-ref
-        order: 1
-
-      &:nth-child(even)
-        background: rgba(grey, .2)
 </style>
 
 <script>
   export default {
-    methods: {
-      order () {
-        this.$refs.item.forEach((v, i) => {
-          v.style.order = i + 2
-        })
-      },
-
-      next () {
-        this.reset()
-        let item = this.$refs.item.shift()
-        this.$refs.item.push(item)
-        this.order()
-        this.reverse = false
-        this.transition()
-      },
-
-      prev () {
-        this.reset()
-        let item = this.$refs.item.pop()
-        this.$refs.item.unshift(item)
-        this.order()
-        this.reverse = true
-        this.transition()
-      },
-
-      reset () {
-        this.$refs.item.forEach(i => {
-          i.removeAttribute('style')
-        })
-      },
-
-      transition () {
-        this.set = false
-        setTimeout(() => this.set = true, 50)
-      }
-    },
-
     data () {
       return {
-        set: true,
-        reverse: false,
         doc: {
           intro: 'Soon',
           types: [
@@ -98,7 +25,6 @@
           ],
           params: []
         },
-        current: 0,
         items: [
           {
             src: 'http://hddesktopwallpapers.in/wp-content/uploads/2015/09/cute-almonds-picture-1440x500.jpg'
@@ -108,6 +34,9 @@
           },
           {
             src: 'https://burlingtonontariobirder.files.wordpress.com/2015/03/cropped-red-tailed-hawk-my-favourite-picture.jpg'
+          },
+          {
+            src: 'http://www.mrwallpaper.com/wallpapers/Space-Planet-Aurora-1366x768.jpg'
           }
         ]
       }
@@ -115,9 +44,6 @@
 
     mounted () {
       this.$emit('view', 'Slider')
-
-      this.$refs.item.unshift(this.$refs.item.pop())
-      this.order()
     }
   }
 </script>
