5579c59c9bbe12b1b3bc80792d4dec7a026d17a3
John Leider <john.j.leider@gmail.com>
2016-12-16 20:03:09 -0500

    removed deprecated code

--- src/views/CardsView.vue
+++ src/views/CardsView.vue
@@ -171,16 +171,12 @@
     data () {
       return {
         doc: {
-          stage: 'iter',
           title: 'Card',
           desc: `
             <p>
               The <code>v-card</code> component is a versatile component that can be used for anything from a panel to a static image. The <strong>card</strong> component has numerous helper components to make markup as easy as possible. Components that have no listed options use <strong class="green--text">Vue's</strong> functional component option for faster rendering and serve as markup sugar to make building easier.
             </p>
           `,
-          types: [
-            'comp', 'slot'
-          ],
           params: [
             [
               '<code>&lt;v-card&gt;</code>',
