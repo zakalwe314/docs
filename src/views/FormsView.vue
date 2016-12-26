0ecfdbe0e4f2abde97da20242cec2b0222878d88
John Leider <john.j.leider@gmail.com>
2016-12-26 15:06:27 -0500

    updated forms documentation

--- src/views/FormsView.vue
+++ src/views/FormsView.vue
@@ -101,22 +101,33 @@
         disabled
       )
     div(slot="markup")
-      markup(lang="xml")
-        |&lt;v-select&gt;
+      markup(lang="html")
+        |&lt;v-select v-bind:options="options"&gt;
         |   ...
         |&lt;/v-select&gt;
-      markup(lang="xml")
-        |&lt;v-text-input&gt;
+        |&nbsp;
+        |&lt;v-text-input label="Label" value="yes" id="terms" name="terms" v-model="text"&gt;
         |   ...
         |&lt;/v-text-input&gt;
-      markup(lang="xml")
-        |&lt;v-radio&gt;
+        |&nbsp;
+        |&lt;v-radio gap indeterminate&gt;
         |   ...
         |&lt;/v-radio&gt;
-      markup(lang="xml")
-        |&lt;v-checkbox&gt;
+        |&nbsp;
+        |&lt;v-checkbox name="check" id="check"&gt;
         |   ...
         |&lt;/v-checkbox&gt;
+      markup(lang="js")
+        |data () {
+        |   return {
+        |     text: '',
+        |     options: [
+        |       { value: 1, text: 'One' },
+        |       { value: 2, text: 'Two' },
+        |       { value: 3, text: 'Three' },
+        |     ]
+        |   }
+        |}
 </template>
 
 <script>
@@ -124,12 +135,8 @@
     data () {
       return {
         doc: {
-          stage: 'iter',
-          title: 'Forms',
-          desc: 'Soon',
-          types: [
-            'comp'
-          ],
+          title: 'Form Inputs',
+          desc: 'Form components are used for collecting user provided information.',
           params: [
             [
               '<code>v-select</code>',
@@ -137,9 +144,34 @@
               'Base component'
             ],
             [
+              '<code>defaultText</code>',
+              'Sets default option text',
+              'Required: Select...'
+            ],
+            [
+              '<code>multiple</code>',
+              'Sets the multiple attribute',
+              'Default: false'
+            ],
+            [
+              '<code>options</code>',
+              'The options array',
+              'Allowed object properties: [id, text]'
+            ],
+            [
               '<code>v-text-input</code>',
               '',
               'Base component'
+            ],
+            [
+              '<code>v-model</code>',
+              '',
+              'Required: false'
+            ],
+            [
+              '<code>type</code>',
+              'Sets the type attribute (v-text-input only)',
+              "Default: text"
             ],
             [
               '<code>v-radio</code>',
@@ -147,9 +179,69 @@
               'Base component'
             ],
             [
+              '<code>v-model</code>',
+              '',
+              'Allowed types: [String, Number, Boolean]'
+            ],
+            [
+              '<code>gap</code>',
+              'Applies the gap class',
+              'Default: false'
+            ],
+            [
               '<code>v-checkbox</code>',
               '',
               'Base component'
+            ],
+            [
+              '<code>v-model</code>',
+              '',
+              'Required: false'
+            ],
+            [
+              '<code>filled</code>',
+              'Applies the filled class',
+              'Default: false'
+            ],
+            [
+              '<code>gap</code>',
+              'Applies the gap class',
+              'Default: false'
+            ],
+            [
+              '<code>indeterminate</code>',
+              'Applies the indeterminate class',
+              'Default: false'
+            ],
+            [
+              '<strong>All Components</strong>',
+              '',
+              ''
+            ],
+            [
+              '<code>disabled</code>',
+              'Sets the disabled attribute',
+              "Default: ''"
+            ],
+            [
+              '<code>label</code>',
+              'Sets the label text',
+              "Default: ''"
+            ],
+            [
+              '<code>id</code>',
+              'Sets the input id',
+              "Default: ''"
+            ],
+            [
+              '<code>name</code>',
+              'Sets the label text',
+              "Default: ''"
+            ],
+            [
+              '<code>placeholder</code>',
+              'Sets the placeholder text',
+              "Default: ''"
             ]
           ]
         },
