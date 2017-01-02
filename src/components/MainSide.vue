25a0f367c17c3313b35bb5f6c6cb7e6013a7a337
John Leider <john.j.leider@gmail.com>
2017-01-02 17:29:51 -0500

    [build]

--- src/components/MainSide.vue
+++ src/components/MainSide.vue
@@ -14,7 +14,7 @@
           width="100"
           alt="Vuetify Logo"
         )
-      a(href="https://github.com/vuetifyjs/vuetify/releases/tag/v0.8.3" target="_blank") v0.8.3
+      a(href="https://github.com/vuetifyjs/vuetify/releases/tag/v0.8.4" target="_blank") v0.8.4
       div(class="sidebar__links")
         a(href="https://github.com/vuetifyjs/vuetify" target="_blank")
           img(src="~public/github.png" alt="github")
