6ce5b2ebc813afb3e5b5b8642d554b15c2d7ca5b
John Leider <john.j.leider@gmail.com>
2017-01-08 18:30:41 -0500

    updated process for building routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,40 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import AboutView from '../views/AboutView.vue'
-import QuickStartView from '../views/QuickStartView.vue'
-import OverviewView from '../views/OverviewView.vue'
-import AlertsView from '../views/AlertsView.vue'
-import BadgesView from '../views/BadgesView.vue'
-import BreadcrumbsView from '../views/BreadcrumbsView.vue'
-import ButtonsView from '../views/ButtonsView.vue'
-import CardsView from '../views/CardsView.vue'
-import ChipsView from '../views/ChipsView.vue'
-import CollapsibleView from '../views/CollapsibleView.vue'
-import ColorsView from '../views/ColorsView.vue'
-import ContentView from '../views/ContentView.vue'
-import DropdownsView from '../views/DropdownsView.vue'
-import GridView from '../views/GridView.vue'
-import FormsView from '../views/FormsView.vue'
-import FooterView from '../views/FooterView.vue'
-import IconView from '../views/IconView.vue'
-import ListsView from '../views/ListsView.vue'
-import ModalsView from '../views/ModalsView.vue'
-import NavbarView from '../views/NavbarView.vue'
-import ParallaxView from '../views/ParallaxView.vue'
-import PaginationView from '../views/PaginationView.vue'
-import ProgressCircularView from '../views/ProgressCircularView.vue'
-import RipplesView from '../views/RipplesView.vue'
-import SliderView from '../views/SliderView.vue'
-import SidebarView from '../views/SidebarView.vue'
-import TabsView from '../views/TabsView.vue'
-import TableView from '../views/TableView.vue'
-import ToastsView from '../views/ToastsView.vue'
-import TooltipsView from '../views/TooltipsView.vue'
-import TypographyView from '../views/TypographyView.vue'
-import HelpersView from '../views/HelpersView.vue'
-import LayoutsView from '../views/LayoutsView.vue'
-import EventBusView from '../views/EventBusView.vue'
+function route (path, view) {
+  return {
+    path: path,
+    component: require(`../views/${view}View.vue`)
+  }
+}
 
 Vue.use(Router)
 
@@ -43,40 +15,37 @@ const router = new Router({
   mode: 'history',
   scrollBehavior: () => ({ y: 0 }),
   routes: [
-    { path: '/', component: AboutView, meta: { test: 'yes' } },
-    { path: '/quick-start', component: QuickStartView },
-    { path: '/overview', component: OverviewView },
-    { path: '/components/alerts', component: AlertsView },
-    { path: '/components/breadcrumbs', component: BreadcrumbsView },
-    { path: '/components/buttons', component: ButtonsView },
-    { path: '/components/cards', component: CardsView },
-    { path: '/components/chips', component: ChipsView },
-    { path: '/components/collapsible', component: CollapsibleView },
-    { path: '/components/dropdowns', component: DropdownsView },
-    { path: '/components/forms', component: FormsView },
-    { path: '/components/footer', component: FooterView },
-    { path: '/components/lists', component: ListsView },
-    { path: '/components/icons', component: IconView },
-    { path: '/components/modals', component: ModalsView },
-    { path: '/components/navbar', component: NavbarView },
-    { path: '/components/pagination', component: PaginationView },
-    { path: '/components/parallax', component: ParallaxView },
-    { path: '/components/progress-circular', component: ProgressCircularView },
-    { path: '/components/sidebar', component: SidebarView },
-    { path: '/components/slider', component: SliderView },
-    { path: '/components/tabs', component: TabsView },
-    { path: '/functions/toasts', component: ToastsView },
-    { path: '/directives/badges', component: BadgesView },
-    { path: '/directives/ripples', component: RipplesView },
-    { path: '/directives/tooltips', component: TooltipsView },
-    { path: '/css/typography', component: TypographyView },
-    { path: '/css/content', component: ContentView },
-    { path: '/css/grid', component: GridView },
-    { path: '/css/colors', component: ColorsView },
-    { path: '/css/tables', component: TableView },
-    { path: '/css/helpers', component: HelpersView },
-    { path: '/layouts', component: LayoutsView },
-    { path: '/event-bus', component: EventBusView },
+    route('/', 'About'),
+    route('/quick-start', 'QuickStart'),
+    route('/overview', 'Overview'),
+    route('/components/alerts', 'Alerts'),
+    route('/components/breadcrumbs', 'Breadcrumbs'),
+    route('/components/buttons', 'Buttons'),
+    route('/components/cards', 'Cards'),
+    route('/components/chips', 'Chips'),
+    route('/components/collapsible', 'Collapsible'),
+    route('/components/dropdowns', 'Dropdowns'),
+    route('/components/forms', 'Forms'),
+    route('/components/lists', 'Lists'),
+    route('/components/icons', 'Icon'),
+    route('/components/modals', 'Modals'),
+    route('/components/navbar', 'Navbar'),
+    route('/components/pagination', 'Pagination'),
+    route('/components/parallax', 'Parallax'),
+    route('/components/progress-circular', 'ProgressCircular'),
+    route('/components/sidebar', 'Sidebar'),
+    route('/components/slider', 'Slider'),
+    route('/components/tabs', 'Tabs'),
+    route('/functions/toasts', 'Toasts'),
+    route('/directives/ripples', 'Ripples'),
+    route('/directives/tooltips', 'Tooltips'),
+    route('/css/typography', 'Typography'),
+    route('/css/content', 'Content'),
+    route('/css/grid', 'Grid'),
+    route('/css/tables', 'Table'),
+    route('/helpers/spacing', 'Spacing'),
+    route('/layouts', 'Layouts'),
+    route('/event-bus', 'EventBus'),
     { path: '*', redirect: '/' }
   ]
 })
