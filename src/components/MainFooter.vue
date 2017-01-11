c4f99755fc95d3b52288e8dc03ae6d4d4a95e07c
John Leider <john.j.leider@gmail.com>
2017-01-10 22:30:59 -0500

    added social buttons to footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -1,4 +1,34 @@
 <template lang="pug">
   v-footer(class="grey darken-2 white--text")
-    div(class="text-xs-right") &copy; 2016 <a class="white--text" href="http://johnleider.com" target="_blank">John Leider</a>
+    div(class="social")
+      div(class="fb-like" data-href="https://vuetifyjs.com/" data-width="30" data-layout="button_count" data-action="like" data-size="small" data-show-faces="false" data-share="true")
+      a(href="https://twitter.com/vuetifyjs" class="twitter-follow-button" data-show-count="false") Follow @vuetifyjs
+    div(class="copy") 
+      |&copy; 2016 <a class="white--text" href="http://johnleider.com" target="_blank">John Leider</a>
 </template>
+
+<style lang="stylus">
+  .footer
+    position: relative
+    
+  .copy
+    text-align: left
+    padding-left: 1rem
+      
+  .social
+    position: absolute !important
+    display: flex !important
+    align-items: center
+    top: 0px
+    right: 25px
+    height: 100%
+    
+  .fb-like
+    display: flex !important
+    align-items: center
+    margin: 0 30px
+    
+  @media screen and (max-width: 768px)
+    .copy
+      display: none
+</style>
