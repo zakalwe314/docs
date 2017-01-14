0f1a88cd0ad5a27005b991a3cc164772f620ec75
John Leider <john.j.leider@gmail.com>
2017-01-14 11:17:12 -0500

    updated table documentation to reflect new overflow component

--- src/views/TableView.vue
+++ src/views/TableView.vue
@@ -1,12 +1,24 @@
 <template lang="pug">
-  div(class="view" id="table-view")
-    div
-      section-def
-        dt(slot="title") {{ doc.title }}
-        dd(slot="desc") {{ doc.desc }}
-    section
-      section-header Examples
-      component-example(header="Default" file="tables/1")
+  doc-view(v-bind:doc="doc" id="table-view")
+    component-example(header="Default" file="tables/1")
+      table
+        thead
+          tr
+            th
+            th(v-for="header in headers" v-text="header")
+            th
+        tbody
+          template(v-for="(item, index) in items")
+            tr
+              td
+                v-checkbox(v-bind:id="'checkbox' + index" filled class="text-xs-center")
+              td(v-for="data in item" v-text="data")
+              td
+                v-btn(icon)
+                  v-icon edit
+    component-example(header="Inside Card" file="tables/2" id="table-overflow")
+      section-text(slot="details") Cards allow tables to overflow-x, creating a scroll-bar if the content is too wide.
+      v-card
         table
           thead
             tr
@@ -17,53 +29,35 @@
             template(v-for="(item, index) in items")
               tr
                 td
-                  v-checkbox(v-bind:id="'checkbox' + index" filled class="text-xs-center")
+                  v-checkbox(v-bind:id="'checkbox2' + index" filled class="text-xs-center")
                 td(v-for="data in item" v-text="data")
                 td
                   v-btn(icon)
                     v-icon edit
-      component-example(header="Inside Card" file="tables/2" id="table-overflow")
-        section-text(slot="details") Cards allow tables to overflow-x, creating a scroll-bar if the content is too wide.
-        v-card
-          table
-            thead
-              tr
-                th
-                th(v-for="header in headers" v-text="header")
-                th
-            tbody
-              template(v-for="(item, index) in items")
-                tr
-                  td
-                    v-checkbox(v-bind:id="'checkbox2' + index" filled class="text-xs-center")
-                  td(v-for="data in item" v-text="data")
-                  td
-                    v-btn(icon)
-                      v-icon edit
-      component-example(header="With Dropdown" file="tables/3")
-        table
-          thead
+    component-example(header="With Dropdown" file="tables/3")
+      table
+        thead
+          tr
+            th
+            th(v-for="header in headers" v-text="header")
+            th
+        tbody
+          template(v-for="(item, index) in items")
             tr
-              th
-              th(v-for="header in headers" v-text="header")
-              th
-          tbody
-            template(v-for="(item, index) in items")
-              tr
-                td
-                  v-checkbox(v-bind:id="'checkbox3' + index" filled class="text-xs-center")
-                td(v-for="data in item" v-text="data")
-                td
-                  v-btn(v-dropdown="{ value: 'dropdown3-' + index }") Options
-                  v-dropdown(
-                    v-bind:id="'dropdown3-' + index"
-                    transition="v-slide-y-transition"
-                    origin="top center"
-                    top right
-                  )
-                    v-dropdown-item(v-bind:item="{ text: 'Edit', href: 'javascript:;' }")
-                    v-dropdown-item(v-bind:item="{ text: 'Reset Password', href: 'javascript:;' }")
-                    v-dropdown-item(v-bind:item="{ text: 'Delete', href: 'javascript:;' }")
+              td
+                v-checkbox(v-bind:id="'checkbox3' + index" filled class="text-xs-center")
+              td(v-for="data in item" v-text="data")
+              td
+                v-btn(v-dropdown="{ value: 'dropdown3-' + index }") Options
+                v-dropdown(
+                  v-bind:id="'dropdown3-' + index"
+                  transition="v-slide-y-transition"
+                  origin="top center"
+                  top right
+                )
+                  v-dropdown-item(v-bind:item="{ text: 'Edit', href: 'javascript:;' }")
+                  v-dropdown-item(v-bind:item="{ text: 'Reset Password', href: 'javascript:;' }")
+                  v-dropdown-item(v-bind:item="{ text: 'Delete', href: 'javascript:;' }")
 </template>
 
 <script>
@@ -74,7 +68,14 @@
       return {
         doc: {
           title: 'Table',
-          desc: 'Tables are useful for displaying large rows of data. This is very common in CRUD (Create Read Update Delete) applications.'
+          desc: 'Tables are useful for displaying large rows of data. This is very common in CRUD (Create Read Update Delete) applications.',
+          functional: {
+            'v-table-overflow': {
+              params: [
+                ['v-table-overflow', '.table__overflow']
+              ]
+            }
+          }
         },
         headers: ['ID', 'Name', 'Email'],
         items: [
