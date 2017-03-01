4aed8933ccd8d4be1d066ca753af14c7ad70442f
John Leider <john.j.leider@gmail.com>
2017-02-28 22:53:01 -0500

    updates

--- src/views/TooltipsView.vue
+++ src/views/TooltipsView.vue
@@ -1,13 +1,50 @@
 <template lang="pug">
   doc-view(v-bind:doc="doc")
     //- component-example(file="tooltips/1")
-    v-stepper
+    v-stepper(v-model="step")
+      v-stepper-header
+        v-stepper-step(step="1" editable) Select campaign settings
+        v-divider
+        v-stepper-step(step="2") Create an ad group
+          small Optional
+        v-divider
+        v-stepper-step(step="3") Create an ad
+      v-stepper-content(step="1")
+        p Step 1
+        v-btn(flat) Cancel
+        v-btn(primary light v-on:click.native="step = 2") Continue
+      v-stepper-content(step="2")
+        p Step 2
+        v-btn(flat) Cancel
+        v-btn(primary light v-on:click.native="step = 3") Continue
+      v-stepper-content(step="3")
+        p Step 3
+        v-btn(flat) Cancel
+        v-btn(primary light v-on:click.native="step = 1") Continue
+    v-stepper(v-model="step" vertical class="mt-5")
+      v-stepper-step(step="1" editable) Select campaign settings
+      v-stepper-content(step="1")
+        p Step 1
+        v-btn(flat) Cancel
+        v-btn(primary light v-on:click.native="step = 2") Continue
+      v-stepper-step(step="2") Create an ad group
+        small Optional
+      v-stepper-content(step="2")
+        p Step 2
+        v-btn(flat) Cancel
+        v-btn(primary light v-on:click.native="step = 3") Continue
+      v-stepper-step(step="3") Create an ad
+      v-stepper-content(step="3")
+        p Step 3
+        v-btn(flat) Cancel
+        v-btn(primary light v-on:click.native="step = 1") Continue
 </template>
 
 <script>
   export default {
     data () {
       return {
+        step: 2,
         doc: {
           title: 'Tooltip',
           desc: 'The <code>v-tooltip</code> directive is useful for conveying information when a user hovers over an element.',
