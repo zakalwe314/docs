84567335357eae743dbc5668404dc4e66e0ce74a
John Leider <jleider@fastforwardacademy.com>
2017-05-01 12:55:00 -0400

    fixed example

--- public/examples/layouts/6.vue
+++ public/examples/layouts/6.vue
@@ -33,7 +33,7 @@
       </v-list>
     </v-navigation-drawer>
     <v-toolbar fixed class="teal">
-      <v-toolbar-side-icon @click.native="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Toolbar</v-toolbar-title>
     </v-toolbar>
     <main>
